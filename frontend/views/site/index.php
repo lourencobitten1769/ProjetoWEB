7fc5d2f28503f98bb9a53f55f846e7f82e5219fe
lourencoG <73118202+lourencoG@users.noreply.github.com>
2021-11-09 12:59:33 +0000

    Design index(frontend)

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -6,46 +6,95 @@ $this->title = 'My Yii Application';
 ?>
 <div class="site-index">
 
-    <div class="jumbotron text-center bg-transparent">
-        <h1 class="display-4">Congratulations!</h1>
-
-        <p class="lead">You have successfully created your Yii-powered application.</p>
-
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
-    </div>
-
     <div class="body-content">
+        <div>
+            <input type="text" placeholder="Search.." style="margin-left: 90%;border-radius: 40px">
+        </div>
+        <br>
 
         <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
+            <div class="col-lg-3">
+                <div class="card" style="width: 200px">
+                    <img src="images/logo.jfif" alt="Avatar" style="width:100%">
+                    <div class="container">
+                        <h4><b>Product Name</b></h4>
+                        <p>Product Description</p>
+                        <p>Product Price</p>
+                    </div>
+                </div>
             </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
+            <div class="col-lg-3">
+                <div class="card" style="width: 200px">
+                    <img src="images/logo.jfif" alt="Avatar" style="width:100%">
+                    <div class="container">
+                        <h4><b>Product Name</b></h4>
+                        <p>Product Description</p>
+                        <p>Product Price</p>
+                    </div>
+                </div>
             </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+            <div class="col-lg-3">
+                <div class="card" style="width: 200px">
+                    <img src="images/logo.jfif" alt="Avatar" style="width:100%">
+                    <div class="container">
+                        <h4><b>Product Name</b></h4>
+                        <p>Product Description</p>
+                        <p>Product Price</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="card" style="width: 200px">
+                    <img src="images/logo.jfif" alt="Avatar" style="width:100%">
+                    <div class="container">
+                        <h4><b>Product Name</b></h4>
+                        <p>Product Description</p>
+                        <p>Product Price</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <br>
+        <div class="row">
+            <div class="col-lg-3">
+                <div class="card" style="width: 200px">
+                    <img src="images/logo.jfif" alt="Avatar" style="width:100%">
+                    <div class="container">
+                        <h4><b>Product Name</b></h4>
+                        <p>Product Description</p>
+                        <p>Product Price</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="card" style="width: 200px">
+                    <img src="images/logo.jfif" alt="Avatar" style="width:100%">
+                    <div class="container">
+                        <h4><b>Product Name</b></h4>
+                        <p>Product Description</p>
+                        <p>Product Price</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="card" style="width: 200px">
+                    <img src="images/logo.jfif" alt="Avatar" style="width:100%">
+                    <div class="container">
+                        <h4><b>Product Name</b></h4>
+                        <p>Product Description</p>
+                        <p>Product Price</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="card" style="width: 200px">
+                    <img src="images/logo.jfif" alt="Avatar" style="width:100%">
+                    <div class="container">
+                        <h4><b>Product Name</b></h4>
+                        <p>Product Description</p>
+                        <p>Product Price</p>
+                    </div>
+                </div>
             </div>
         </div>
 
