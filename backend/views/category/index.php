9c6ef80857b7de3e102c787a243039ea18b5a014
Tomás Cândido <2201142@my.ipleiria.pt>
2021-12-03 13:46:10 +0000

    Table Categories

--- backend/views/category/index.php
+++ backend/views/category/index.php
@@ -9,6 +9,8 @@ use yii\widgets\Pjax;
 
 $this->title = 'Categories';
 $this->params['breadcrumbs'][] = $this->title;
+\backend\assets\AppAsset::register($this);
+
 ?>
 <div class="categories-index">
 
@@ -33,6 +35,75 @@ $this->params['breadcrumbs'][] = $this->title;
             ['class' => 'yii\grid\ActionColumn'],
         ],
     ]); */?>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-offset-1 col-md-10">
+                <div class="panel">
+                    <div class="panel-heading">
+                        <div class="row">
+                            <div class="col-sm-12 col-xs-12">
+                                <h4 class="title pull-left">Users List</h4>
+                                <a href="?r=category%2Fcreate" class="btn btn-sm btn-primary pull-right"><i class="fa fa-plus"></i> Create Category</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="panel-body table-responsive">
+                        <table class="table">
+                            <thead>
+                            <tr>
+                                <th>Category ID</th>
+                                <th>Category Name</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <td colspan="5"><input type="text" class="form-control" placeholder="Search Categories"></td>
+                            </tr>
+                            <?php
+                            foreach ($categories as $category){?>
+                                <tr>
+                                    <td><?php echo $category->category_id?></td>
+                                    <td><?php echo $category->category?></td>
+                                    <td>
+                                        <ul class="action-list">
+                                            <li><a href="?r=category%2Fview&category_id=<?php echo $category->category_id?>" class="btn btn-success"><i class="fa fa-search"></i></a></li>
+                                            <li><a href="?r=category%2Fupdate&category_id=<?php echo $category->category_id?>" class="btn btn-primary"><i class="fa fa-pencil-alt"></i></a></li>
+                                            <li><a href="?r=category%2Fdelete&category_id=<?php echo $category->category_id?>" class="btn btn-danger"><i class="fa fa-trash"></i></a></li>
+                                        </ul>
+                                    </td>
+                                </tr>
+                                <?php
+                            }
+                            ?>
+
+                            </tbody>
+                        </table>
+                    </div>
+                    <div class="panel-footer">
+                        <div class="row">
+                            <div class="col-sm-6 col-xs-6">showing <b>5</b> out of <b>25</b> entries</div>
+                            <div class="col-sm-6 col-xs-6">
+                                <ul class="pagination hidden-xs pull-right">
+                                    <li><a href="#">«</a></li>
+                                    <li class="active"><a href="#">1</a></li>
+                                    <li><a href="#  ">2</a></li>
+                                    <li><a href="#">3</a></li>
+                                    <li><a href="#">4</a></li>
+                                    <li><a href="#">5</a></li>
+                                    <li><a href="#">»</a></li>
+                                </ul>
+                                <ul class="pagination visible-xs pull-right">
+                                    <li><a href="#">«</a></li>
+                                    <li><a href="#">»</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
     <a href="?r=category%2Fview&category_id=1">View</a>
 
